d93f98207b78faffbf2e2901bc8c1b866555d9fe
SandeepKumarNishad <105138121+SandeepKumarNishad@users.noreply.github.com>
2022-12-30 20:22:23 -0800

    Add files via upload

---
+++ o2_datatype.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all js code as newer version
+//alert(3+3)//we are using nodejs,not browser
+console.log(3 +
+        3) //code readability should be high
+console.log("hitesh")
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state;
+//number
+//bigint
+//string=""
+//boolean>> true/false
+//null=>standalone value
+//undefine=>
+//symbol=>unique
+//
+
+//Object
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
+//
+
+/*
+explore data type in ecma website 
+*/
